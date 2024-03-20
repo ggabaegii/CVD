84a7b0f0f5bc87bf9148999c9758362622286c8b
김소희 <chyhwap01@naver.com>
2024-03-21 03:35:42 +0900

    24.03.21 UI 및 갤러리에서 사진 불러오는 기능까지 완료

---
+++ app/src/main/java/com/example/cvd/auto.java
@@ -0,0 +1,16 @@
+package com.example.cvd;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class auto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auto);
+
+
+    }
+}
