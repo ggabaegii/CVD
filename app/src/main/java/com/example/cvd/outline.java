23927631d98130f38d8146a6ebfd1a7645fc176b
wkdgydms9121 <84010233+wkdgydms9121@users.noreply.github.com>
2024-05-22 22:45:31 +0900

    ui outline 수정
    outline 수정

--- app/src/main/java/com/example/cvd/outline.java
+++ app/src/main/java/com/example/cvd/outline.java
@@ -1,8 +1,8 @@
 package com.example.cvd;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class outline extends AppCompatActivity {
 
